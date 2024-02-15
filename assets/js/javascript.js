e08da82ad098600bf7af364e18c6aeba476d76ec
freeram <kulikovpavel2801@gmail.com>
2024-02-14 18:55:31 -0700

    Add smooth scrolling

---
+++ assets/js/javascript.js
@@ -0,0 +1,22 @@
+const headerLinks = document.querySelectorAll('.header-links a');
+const scrollBtn = document.getElementById("down-btn");
+const aboutMe = document.getElementById("about-me");
+
+
+scrollBtn.addEventListener('click', () => {
+    aboutMe.scrollIntoView({ behavior: 'smooth' });
+});
+
+headerLinks.forEach(link => {
+    link.addEventListener('click', function (e) {
+        // Do not send to section non-smoothly
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        if (targetElement) {
+            targetElement.scrollIntoView({ behavior: 'smooth' });
+        }
+    });
+});
